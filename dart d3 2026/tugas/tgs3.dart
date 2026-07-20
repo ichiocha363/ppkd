80625ee9e00a0d7365e90d7e59d002ef12cc6535
zhilly <zhillyhilmansyah@gmail.com>
2026-07-20 12:02:56 +0700

    tugas 3 baru

---
+++ dart d3 2026/tugas/tgs3.dart
@@ -0,0 +1,59 @@
+void bilanganGanjil(){
+    for (int i = 1; i<=20; i++){
+       if (i % 2 !=0)(
+        print("bilangan ganjil : $i")
+       );
+      
+    }
+}
+
+// void cetakKarakter (){
+//     String hasil = "";
+
+//   for (int i = 1; i <= 5; i++) {
+//     hasil += "* ";
+//   }
+
+//   print(hasil);
+// }
+
+void cetakbawah(){
+    int i = 1;
+    while (i <= 5){
+        print ("*");
+        i++;
+    }
+}
+
+void namaBerulang(){
+    int i = 1;
+    while (i <= 4){
+        print ("zhilly");
+        i++;
+    }
+}
+
+void perulanganList(){
+    List<String> buah = ["Apel","Jeruk","Mangga","Anggur"];
+
+    for (var n in buah) {
+        print ("saya suka $n");
+    }
+}
+
+void listDaftarBelanja(){
+    List<String> barang = ["beras","Minyak","gula","telur"];
+
+    for (var bn in barang) {
+        print ("item ke-${barang.indexOf(bn)+1}: $bn");
+    }
+}
+
+void main(){
+    bilanganGanjil();
+    // cetakKarakter();
+    namaBerulang();
+    perulanganList();
+    listDaftarBelanja();
+    cetakbawah();
+}
